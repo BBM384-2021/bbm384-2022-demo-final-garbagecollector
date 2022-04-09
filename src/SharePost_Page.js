8a69f45b70fba7667a2b0aed99fb18a104684af5
Pelin Fildiş <56741226+b21827412@users.noreply.github.com>
2022-04-09 22:40:20 +0300

    Add files via upload

---
+++ src/SharePost_Page.js
@@ -0,0 +1,4 @@
+// This class ia made for containing utiliy functions for sharing posts.
+//Such as a funciton gives a pop-up screen for selecting post type or
+// opening a pop-up screen with details when a user clicks an existing post.
+let a  = 5;
