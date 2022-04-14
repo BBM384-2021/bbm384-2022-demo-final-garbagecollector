93da2d512e2209c2e1023570e576431a9cb7c92e
Abdulkadir Alaca <76223197+abdulkadiralaca@users.noreply.github.com>
2022-04-14 20:44:14 +0300

    new files created
    file directories was changed

---
+++ LinkedHUDemoApplication/src/main/java/com/LinkedHU/demo/service/UserService.java
@@ -0,0 +1,17 @@
+package com.LinkedHU.demo.service;
+import com.LinkedHU.demo.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    public User saveUser(User user);
+    public List<User> getAll();
+    public User login(User user);
+    public void deleteUserById(User user);
+    void editUserById(User user);
+
+    // security checks
+    boolean checkUsername(String username);
+    boolean checkEmail(String email);
+    boolean checkPassword(String password);
+}
---
+++ LinkedHUDemoApplication/src/test/java/com/LinkedHU/demo/LinkedHUDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LinkedHU.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkedHUDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
