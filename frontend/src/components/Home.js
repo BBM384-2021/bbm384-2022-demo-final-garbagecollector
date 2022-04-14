6f1d57061cc7dedb043163f9de193b5ded3a2782
Pelin Fildiş <56741226+b21827412@users.noreply.github.com>
2022-04-15 00:03:15 +0300

    Add files via upload

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,14 @@
+import logo from '../logo.svg';
+import '../App.css';
+
+import Register from './Register';
+
+function Home() {
+    return (
+        <div className="App">
+            <Register/>
+        </div>
+    )
+}
+
+export default Home
