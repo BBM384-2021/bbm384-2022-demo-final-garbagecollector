7effe7c244aa4e9439b8bc1ecd0ca29a5a67576e
Cagatay <79810439+cagatayer-11@users.noreply.github.com>
2022-04-25 22:26:09 +0300

    Add files via upload

---
+++ src/test/java/com/app/linkedhu/LinkedHuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.linkedhu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkedHuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
