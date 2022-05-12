2c648b560e10fcfe23050efa5b8a28c93f2e0e70
cagatayer-11 <cagatayer.2000@gmail.com>
2022-05-13 00:58:07 +0300

    excel

--- src/main/java/com/app/linkedhu/controller/AdminController.java
+++ src/main/java/com/app/linkedhu/controller/AdminController.java
@@ -1,10 +1,15 @@
 package com.app.linkedhu.controller;
 
 import com.app.linkedhu.entitites.User;
+import com.app.linkedhu.config.UserExcelExporter;
 import com.app.linkedhu.service.UserService;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 @CrossOrigin
 @RestController
@@ -23,6 +28,22 @@ public class AdminController {
         return userService.getAllDisabled();
     }
 
+    @GetMapping("/excel")
+    public void exportToExcel(HttpServletResponse response) throws IOException {
+        response.setContentType("application/octet-stream");
+        DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
+        String currentDateTime = dateFormatter.format(new Date());
+
+        String headerKey = "Content-Disposition";
+        String headerValue = "attachment; filename=users_" + currentDateTime + ".xlsx";
+        response.setHeader(headerKey, headerValue);
+
+        List<User> listUsers = userService.getAllUsers();
+
+        UserExcelExporter excelExporter = new UserExcelExporter(listUsers);
+
+        excelExporter.export(response);
+    }
 
     @PutMapping("/enable")
     public void enableUser(@RequestParam(name = "userId") Long userId){
